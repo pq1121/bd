6d5e4662e47fe5fbb0db3fd68ca097e9c5631e72
Aleksey <pq1121@mail.ru>
2025-05-04 21:18:03 +0300

    add last task_1 and task_2

--- hw_1/practice_1_task_1.sql
+++ hw_1/practice_1_task_1.sql
@@ -1,7 +1,7 @@
 USE seschool_02;
 
 CREATE TABLE `MonstersIncEmployees` (
-	`EmployeeID` BIGINT(8) UNSIGNED NOT NULL PRIMARY KEY,
+	`EmployeeID` BIGINT(8) UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
 	`MonsterName` VARCHAR(75) NOT NULL,
 	`Type` ENUM('Монстр','Помощник','Администратор','Уборщик','Учитель') NOT NULL DEFAULT 'Монстр',
 	`Department` VARCHAR(100) NOT NULL DEFAULT 'Нулевой отдел',
---
+++ hw_1/practice_1_task_add_1.sql
@@ -0,0 +1,20 @@
+USE seschool_02;
+
+INSERT INTO `MonstersIncEmployees`
+	(`EmployeeID`, `MonsterName`, `Department`, `ScareLevel`) VALUES
+	(1,'Mike Wazowski', 'Отдел Пуганий', 7);
+	
+INSERT INTO `MonstersIncEmployees`
+	(`MonsterName`, `Department`, `ScareLevel`) VALUES
+	('James P. Sullivan', 'Отдел Пуганий', 10),
+	('Randall Boggs', 'Отдел Пуганий', 8),
+	('George Sanderson', 'Отдел Пуганий', 5);
+
+INSERT INTO `MonstersIncEmployees`
+	(`MonsterName`, `Type`, `Department`, `ScareLevel`) VALUES
+	('Celia Mae', 'Администратор', 'Отдел поддержки', 3),
+	('Roz', 'Администратор', 'Отдел документации', 2),
+	('Henry J. Waternoose', 'Администратор', 'Управление', 4),
+	('Fungus', 'Помощник', 'Отдел пуганий', 6),
+	('Needleman', 'Помощник', 'Отдел документации', 4),
+	('Smitty', 'Помощник', 'Отдел поддержки', 5);
