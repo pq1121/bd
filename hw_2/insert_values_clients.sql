bf28f423107910ebd44034ff8c44de80bdb5d390
Aleksey <pq1121@mail.ru>
2025-05-10 17:18:21 +0300

    create_table_clients and add values

---
+++ hw_2/insert_values_clients.sql
@@ -0,0 +1,13 @@
+USE `seschool_02`;
+
+INSERT INTO `Clients` (`first_name`,`last_name`,`city`,`age`,`balance`,`is_active`) VALUES
+	('Ivan', 'Ivanov', 'Moscow', 34, 15200.50, TRUE),
+	('Anna', 'Petrova', 'SaintPetersburg', 28, 8700.00, FALSE),
+	('Olga', 'Sidorova', 'Novosibirsk', 45, 21200.00, TRUE),
+	('Sergey', 'Kuznetsov', 'Moscow', 31, 350.00, FALSE),
+	('Maria', 'Smirnova','Kazan', 22, 4200.00, TRUE),
+	('Dmitry', 'Popov', 'Moscow', 40, 0.00, TRUE),
+	('Alexey', 'Lebedev', 'Omsk', 50, 10800.00, FALSE),
+	('Elena', 'Morozova', 'Kazan', 38, 18750.00, TRUE),
+	('Victor', 'Soloviev', 'Perm', 27, 6400.00, TRUE),
+	('Ekaterina', 'Volkova', 'Moscow', 29, 9900.00, FALSE);
