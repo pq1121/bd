09b859f4d44ff9d3ced5ca78af8a47ab86fa8e82
Aleksey <pq1121@mail.ru>
2025-05-16 09:50:06 +0300

    add insert table zootopia

---
+++ hw_3/INSERT_table_Zootopia.sql
@@ -0,0 +1,17 @@
+USE `seschool_02`;
+
+INSERT INTO `ZootopiaCharacters`
+	(`CharacterName`,`Species`,`Role`,`Occupation`,`Catchphrase`) VALUES
+	('Judy Hopps', 'Кролик', 'Главный герой', 'Полицейский', 'Сначала попробуй!'),
+	('Nick Wilde', 'Лиса', 'Главный герой', 'Полицейский', 'Скользкий как лиса!'),
+	('Bellwether', 'Овца', 'Антагонист', 'Мэр', NULL);
+	
+INSERT INTO `ZootopiaCharacters`
+	(`CharacterName`,`Species`,`Occupation`,`Catchphrase`) VALUES
+	('Chief Bogo', 'Буйвол', 'Шеф полиции', NULL),
+	('Flash', 'Ленивец', 'Сотрудник DMV', NULL),
+	('Mr. Big', 'Полярный медведь', 'Босс мафии', 'Твое слово — мое слово'),
+	('Finnick', 'Лиса', 'Мошенник', NULL),
+	('Clawhauser', 'Гепард', 'Сотрудник полиции', 'Добро пожаловать в Зверополис!'),
+	('Duke Weaselton', 'Ласка', 'Контрабандист', NULL),
+	('Yax', 'Як', 'Управляющий спа-салоном', NULL);
